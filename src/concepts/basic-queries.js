7652e231af1bd4a6537cf177f5d9a793c43ed4a5
Yusuf <teajhaney@gmail.com>
2025-08-21 19:37:08 +0100

    basic crud with posygreSQL

--- src/concepts/basic-queries.js
+++ src/concepts/basic-queries.js
@@ -18,8 +18,76 @@ export const createUserTable = async () => {
   }
 };
 
-
-
 export const insertUser = async (username, email) => {
-	
-}
+  const insertQuery = `
+	INSERT INTO users (username, email)
+	VALUES ($1,$2)
+	RETURNING *
+	`;
+
+  try {
+    const res = await db(insertQuery, [username, email]);
+    console.log('User inserted successfully', res.rows[0]);
+
+    return res.rows[0];
+  } catch (error) {
+    console.error('Error inserting user:', error);
+    throw error;
+  }
+};
+
+export const fetchAllUsers = async ({}) => {
+  const fetchAllUser = `SELECT * FROM users`;
+  try {
+    const res = await db(fetchAllUser);
+    console.log('Fetched all users successfully', res.rows);
+    return res.rows;
+  } catch (error) {
+    console.error('Error fetching all users:', error);
+    throw error;
+  }
+};
+
+export const updateUserEmail = async (username, newEmail) => {
+  const updateEmail = `
+	UPDATE users
+	SET email = $2
+	WHERE username= $1
+	RETURNING *
+	`;
+
+  try {
+    const res = await db(updateEmail, [username, newEmail]);
+    if (res.rows.length > 0) {
+      console.log('User email updated successfully', res.rows[0]);
+      return res.rows[0];
+    } else {
+      console.log('User not found with given username:', username);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error updating user email:', error);
+    throw error;
+  }
+};
+
+export const deleteUser = async username => {
+  const deleteUserQuery = `
+	DELETE FROM users
+	WHERE username = $1
+	RETURNING *
+	`;
+  try {
+    const res = await db(deleteUserQuery, [username]);
+    if (res.rows.length > 0) {
+      console.log('User deleted successfully', res.rows[0]);
+      return res.rows[0];
+    } else {
+      console.log('User not found with given username:', username);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error deleting user :', error);
+    throw error;
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
-import { createUserTable } from './concepts/basic-queries.js';
+import {
+  createUserTable,
+  insertUser,
+  fetchAllUsers,
+  updateUserEmail,
+  deleteUser,
+} from './concepts/basic-queries.js';
 
 //test basic queries
 
 const textBasicQueries = async () => {
   try {
     // await createUserTable();
+
+    //insert user
+    // await insertUser('john doe', 'john@example.com');
+    // await insertUser('Youju Kang', 'youju@example.com');
+    // await insertUser('Seobi Kang', 'seobikang@example.com');
+    // await insertUser('Tijani Yusuf', 'tijaniyusuf@example.com');
+
+    //fetch all users
+    // console.log('All users');
+    // const allUsers = await fetchAllUsers({});
+    // console.log(allUsers);
+
+    //update user
+    // console.log('Update info');
+    // const updatedUser = await updateUserEmail('john doe', 'john@gmail.con');
+    //   console.log(updatedUser);
+
+    //delete user
+    console.log('Update info');
+    const de = await deleteUser('john doe');
+    console.log(deleteUser);
   } catch (error) {
     console.error('Error in basic queries:', error);
     throw error;
